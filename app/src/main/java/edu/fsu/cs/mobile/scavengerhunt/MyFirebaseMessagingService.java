477034670395649a500c2c6776a705cb6e676293
JavietTSalas <JavierTSalas@users.noreply.github.com>
2018-04-13 00:31:26 -0400

    Added notification receiver to MyFirebaseMessagingService.java

---
+++ app/src/main/java/edu/fsu/cs/mobile/scavengerhunt/MyFirebaseMessagingService.java
@@ -0,0 +1,90 @@
+package edu.fsu.cs.mobile.scavengerhunt;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = MyFirebaseMessagingService.class.getCanonicalName();
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // ...
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            sendSimpleNotifcation(remoteMessage);
+            /*
+            // Check if data needs to be processed by long running job
+            if (true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+                // Handle message within 10 seconds
+                handleNow();
+            }
+             */
+
+        }
+
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            sendNotification(remoteMessage);
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendSimpleNotifcation method below.
+    }
+
+    private void sendNotification(RemoteMessage remoteMessage) {
+        RemoteMessage.Notification notification = remoteMessage.getNotification();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.map_bg))
+                .setSmallIcon(R.drawable.map_bg)
+                .setContentTitle(notification.getTitle())
+                .setContentText(notification.getBody())
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+
+
+    private void sendSimpleNotifcation(RemoteMessage remoteMessage) {
+
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle("ScavengerHunt")
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setAutoCancel(true)
+                .setPriority(Notification.PRIORITY_HIGH)
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000}).build();
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(420, notification);
+    }
+}
